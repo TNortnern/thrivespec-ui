3e7ec6e13de13cdf231c6027a20c03ba4bc6e10f
TNORTHERN <traynorthern@yahoo.com>
2020-06-13 19:22:34 -0500

    account page

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -4,6 +4,9 @@ import Wrapper from "../components/misc/Wrapper";
 import { Image, StyleSheet, View } from "react-native";
 import PageTitle from "../components/misc/PageTitle";
 import { layout, colors } from "../assets/styles";
+import Stats from "../components/home/Stats";
+import { ScrollView } from "react-native-gesture-handler";
+import TextCTA from "../components/home/TextCTA";
 
 const Account = () => {
   const commonMargin = 16;
@@ -16,10 +19,32 @@ const Account = () => {
         }}
         style={{ width: "100%", height: 298 }}
       />
-      <View style={{ ...layout.container, marginTop: commonMargin }}>
+      <ScrollView style={{ ...layout.container, marginTop: commonMargin }}>
         <PageTitle title="michellek1995" />
-        <Text style={{ color: colors.gray, fontSize: 22, marginBottom: commonMargin }}>Michelle Kim</Text>
-      </View>
+        <Text
+          style={{
+            color: colors.gray,
+            fontSize: 22,
+            marginVertical: commonMargin,
+          }}
+        >
+          Michelle Kim
+        </Text>
+        <View style={{ marginBottom: commonMargin }}>
+          <Stats />
+        </View>
+        <TextCTA title="About" cta={{ name: "More" }} />
+        <Text style={{ fontSize: 19 }}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam eligendi
+          cum odio, consequatur incidunt enim. Reprehenderit ratione laudantium
+          distinctio doloremque, nostrum natus iure ut dolorem nisi. Id unde aut
+          perspiciatis!
+        </Text>
+        <TextCTA title="Groups" cta={{ name: "View" }}></TextCTA>
+        <View style={{ paddingBottom: commonMargin }}>
+          <TextCTA title="Liked Recipes" cta={{ name: "View" }}></TextCTA>
+        </View>
+      </ScrollView>
     </Wrapper>
   );
 };
